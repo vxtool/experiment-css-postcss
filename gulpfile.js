4176e4ad9ee100b97ce0e6af428cb9758f78390a
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-01-11 00:55:18 -0200

    added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var postcss    = require('gulp-postcss');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('default', function () {
+    return gulp.src('source/**/*.css')
+        .pipe( sourcemaps.init() )
+        .pipe( postcss([ require('autoprefixer'), require('precss') ]) )
+        .pipe( sourcemaps.write('.') )
+        .pipe( gulp.dest('build/') );
+});
